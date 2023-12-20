e3cdce19d4cad83f6a214195c210fe53a24e80fc
Daniel Fonseca <daniel.fonseca@cinbesa.com.br>
2023-12-20 10:15:13 -0300

    primeiro commit

---
+++ src/main/java/br/com/epet/EpetApplication.java
@@ -0,0 +1,13 @@
+package br.com.epet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/epet/EpetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.epet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
